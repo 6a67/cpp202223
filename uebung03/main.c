1d803bbfa30c7b3ef0a79793e7e7007f3adf7c9c
6a67 <83820531+6a67@users.noreply.github.com>
2022-11-10 12:13:36 +0100

    [uebung03] formatted file

--- uebung03/main.c
+++ uebung03/main.c
@@ -12,39 +12,38 @@
 #include "mainwindow.h"
 #include "plyio.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	/* Our SDL_Window ( just like with SDL2 wihout OpenGL) */
-	SDL_Window *mainWindow;
+    /* Our SDL_Window ( just like with SDL2 wihout OpenGL) */
+    SDL_Window* mainWindow;
 
-	/* Our opengl context handle */
-	SDL_GLContext mainContext;
+    /* Our opengl context handle */
+    SDL_GLContext mainContext;
 
-	if (!createSDLWindow(&mainWindow, &mainContext))
+    if (!createSDLWindow(&mainWindow, &mainContext))
     {
-		return -1;
+        return -1;
     }
 
-	/* Init model struct with default values */
-	Model* model = (Model*) malloc(sizeof(Model));
-	model->numFaces = 0;
-	model->numVertices = 0;
-	model->indexBuffer = NULL;
-	model->vertexBuffer = NULL;
+    /* Init model struct with default values */
+    Model* model        = (Model*) malloc(sizeof(Model));
+    model->numFaces     = 0;
+    model->numVertices  = 0;
+    model->indexBuffer  = NULL;
+    model->vertexBuffer = NULL;
 
-	/*++++++++++++++++++++++++++++++++++++++++++*/
-	/* TODO: Load data from PLY file into model */
-	/*++++++++++++++++++++++++++++++++++++++++++*/
+    /*++++++++++++++++++++++++++++++++++++++++++*/
+    /* TODO: Load data from PLY file into model */
+    /*++++++++++++++++++++++++++++++++++++++++++*/
 
-	/* Call main rendering loop */
-	mainLoop(mainWindow, model);
+    /* Call main rendering loop */
+    mainLoop(mainWindow, model);
 
-	/* Free resources */
-	cleanupSDL(mainWindow, mainContext);
+    /* Free resources */
+    cleanupSDL(mainWindow, mainContext);
 
-	/*++++++++++++++++++++++++++++++++++++++++++*/
-	/* TODO: Free model data                    */
-	/*++++++++++++++++++++++++++++++++++++++++++*/
-	return 0;
+    /*++++++++++++++++++++++++++++++++++++++++++*/
+    /* TODO: Free model data                    */
+    /*++++++++++++++++++++++++++++++++++++++++++*/
+    return 0;
 }
-
