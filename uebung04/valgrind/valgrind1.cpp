5ee74066179df54029026efd70648e868a9436a5
6a67 <83820531+6a67@users.noreply.github.com>
2022-11-17 11:32:26 +0100

    [uebung04] fixed valgrind1.cpp

--- uebung04/valgrind/valgrind1.cpp
+++ uebung04/valgrind/valgrind1.cpp
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc != 2)
     {
         return -1;
     }
 
-    int idx = atoi(argv[1]);
-    char *a = malloc(100);
-    char *b = malloc(1000);
-    a[idx] = 0; // BUG: if idx < 0 or idx >= 100
+    int   idx = atoi(argv[1]);
+    char* a   = (char*) malloc(100);
+    char* b   = (char*) malloc(1000);
+    a[idx]    = 0;  // BUG: if idx < 0 or idx >= 100
     free(a);
     free(b);
     return 0;
