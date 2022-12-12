9f252e7ea5a180d5ddfc7d21da1ad9b63907cda0
6a67 <83820531+6a67@users.noreply.github.com>
2022-12-12 17:43:20 +0100

    [uebung08] merged files

--- uebung08/List.hpp
+++ uebung08/List.hpp
@@ -1,33 +1,56 @@
-#ifndef LIST_HPP
-#define LIST_HPP
+/*
+ * List.hpp
+ *
+ *  @date 02.12.2018
+ *  @author Thomas Wiemann
+ * 
+ *  Copyright (c) 2018 Thomas Wiemann.
+ *  Restricted usage. Licensed for participants of the course "The C++ Programming Language" only.
+ *  No unauthorized distribution.
+ */
+
+
+#ifndef LIST_H
+#define LIST_H
 
 namespace asteroids
 {
-template<typename T>
+
+/**
+ * @brief   A simple generic list class
+ */
+template<typename T> 
 class List
 {
-private:
-    struct Node
-    {
-        Node* next;
-        Node* prev;
-        T     value;
-    };
+   public:
 
-    Node* head;
+      /**
+       * @brief Constructs an empty list.
+       */
+      List();
+      /**
+       * @brief  Destructor. Frees the generated nodes.
+       */
+      ~List();
+      /**
+       * @brief Inserts an item into the list, i.e. a new node
+       *        constaining @ref item is created.
+       * @param item To be inserted
+       *
+       */
+      void insert(T item);
 
-public:
-    List()
-    : head(nullptr)
-    {
-    }
-
-    void insert(T item);
-    void for_each(void (*do_something)(T& item));
-
-    ~List();
+      /**
+       * @brief Iterates over all items in the list and calls
+       * the given function @ref do_something(...) for
+       * every item stored in the list.
+       *
+       * @param do_something Function pointer to apply to all elements.
+       */
+      void for_each(void (*do_something)(T& item));
 };
-}  // namespace asteroids
 
-#include "List.tcc"
+}
+
 #endif
+/* end of include guard: LIST_H */
